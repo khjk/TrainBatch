4b6c7e1433fd8ce5902fe25d4c215991f43938f5
khjk <begywjd@naver.com>
2025-01-29 20:43:39 +0900

    initial commit

---
+++ src/main/java/com/train/trainbatch/TrainBatchApplication.java
@@ -0,0 +1,13 @@
+package com.train.trainbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/train/trainbatch/TrainBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.train.trainbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
