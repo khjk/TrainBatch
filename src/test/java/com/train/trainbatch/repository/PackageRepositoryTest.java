a5756f49a2781fa9e0719419ed849c82120642bb
khjk <begywjd@naver.com>
2025-01-31 15:07:28 +0900

    feat : packageEntity 테스트코드추가

--- src/test/java/com/train/trainbatch/repository/PackageRepositoryTest.java
+++ src/test/java/com/train/trainbatch/repository/PackageRepositoryTest.java
@@ -16,8 +16,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
@@ -56,11 +55,46 @@ public class PackageRepositoryTest {
         packageRepository.save(packageEntity1);
 
         //when
-        final List<PackageEntity> packageEntities = packageRepository.findByCreatedAtAfter(dateTime, PageRequest.of(
-                0, 1, Sort.by("packageSeq").descending()));
+        final List<PackageEntity> packageEntities = packageRepository.findByCreatedAtAfter(dateTime,
+                PageRequest.of(0, 1, Sort.by("packageSeq").descending()));
 
         //then
         assertEquals(1, packageEntities.size());
         assertEquals(packageEntity1.getPackageSeq(), packageEntities.get(0).getPackageSeq());
     }
+
+    @Test
+    public void test_updateCountAndPeriod() {
+        // given
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("바디 챌린지 PT 4개월");
+        packageEntity.setPeriod(90);
+        packageRepository.save(packageEntity);
+
+        // when
+        int updateCount = packageRepository.updateCountAndPeriod(
+                packageEntity.getPackageSeq(), 30, 120);
+        final PackageEntity updatedPackageEntity = packageRepository.findById(packageEntity.getPackageSeq()).get();
+
+        // then
+        assertEquals(1, updateCount);
+        assertEquals(30, updatedPackageEntity.getCount());
+        assertEquals(120, updatedPackageEntity.getPeriod());
+    }
+
+    @Test
+    public void test_delete() {
+        //given
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("제거할거에용");
+        packageEntity.setCount(1);
+        PackageEntity newPackageEntity = packageRepository.save(packageEntity);
+
+        //when
+        packageRepository.deleteById(newPackageEntity.getPackageSeq());
+
+        //then
+        assertTrue(packageRepository.findById(newPackageEntity.getPackageSeq()).isEmpty());
+    }
+
 }
